c577ae663cb3d4e111ec43775316d8d762addabd
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-03 07:16:58 +0900

    ハイスコア更新

--- src/00_main.c
+++ src/00_main.c
@@ -5,8 +5,8 @@ void main(void)
     init_palette();
 
     for (uint8_t i = 0; i < 8; i++) {
-        GV->sc[i] = 7 - i + 1;
-        GV->hi[i] = i + 1;
+        GV->hi[i] = 7 - i + 1;
+        GV->sc[i] = i + 1;
     }
     title();
 }
--- src/03_score.c
+++ src/03_score.c
@@ -5,8 +5,17 @@ void score_print(NameTable* nam)
 {
     uint8_t detectS = 0;
     uint8_t detectH = 0;
+    uint8_t updateH = 0;
     for (uint8_t i = 0; i < 8; i++) {
         // score
+        if (!updateH) {
+            if (GV->hi[7 - i] < GV->sc[7 - i]) {
+                updateH = 1;
+            }
+        }
+        if (updateH) {
+            GV->hi[7 - i] = GV->sc[7 - i];
+        }
         if (7 == i || 0 != detectS || 0 != GV->sc[7 - i]) {
             nam->ptn[2][5 + i] = GV->sc[7 - i] + '0';
             detectS = 1;
