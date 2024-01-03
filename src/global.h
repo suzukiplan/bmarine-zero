7ff8c29aef090ab0be5f7fb073cf26dd23e0d4f2
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-03 09:22:38 +0900

    refactor: global.h を header.h に統合

--- src/global.h
+++
@@ -1,8 +0,0 @@
-// グローバル変数
-
-typedef struct {
-    uint8_t sc[8];  // スコア
-    uint8_t hi[8];  // ハイスコア
-    uint8_t ridx;   // 乱数インデクス
-} GlobalVariables;
-#define GV ((GlobalVariables*)0xC000)
--- src/header.h
+++ src/header.h
@@ -1,7 +1,17 @@
 #include "../vgszero/lib/sdcc/vgs0lib.h"
-#include "global.h"
+
+// グローバル変数
+typedef struct {
+    uint8_t sc[8];  // スコア
+    uint8_t hi[8];  // ハイスコア
+    uint8_t ridx;   // 乱数インデクス
+} GlobalVariables;
+#define GV ((GlobalVariables*)0xC000)
+
+// table宣言変数
 extern const uint16_t random[256];
 
+// サブルーチン
 void init_palette(void);
 void title(void);
 void score_print(NameTable* nam);
