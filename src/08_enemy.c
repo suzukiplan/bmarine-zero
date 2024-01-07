c916cbbb58f698c4c587cc37f8356fd0cc742185
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-08 08:20:12 +0900

    refactor

--- src/08_enemy.c
+++ src/08_enemy.c
@@ -154,15 +154,13 @@ void add_enemy(uint8_t type, uint8_t x, uint8_t y)
 void move_marineLR(Enemy* enemy) __z88dk_fastcall
 {
     enemy->vx.value = 0x00C0;
+    enemy->n8[0]++;
 
     // プロペラを回転
-    enemy->n8[0]++;
-    enemy->n8[0] &= 3;
-    if (0 == enemy->n8[0]) {
+    if (0 == (enemy->n8[0] & 3)) {
         enemy->n8[1]++;
         enemy->n8[1] &= 0x03;
-        uint8_t sn = enemy->si;
-        VGS0_ADDR_OAM[SP_ENEMY + sn].ptn = 0x18 + enemy->n8[1];
+        VGS0_ADDR_OAM[SP_ENEMY + enemy->si].ptn = 0x18 + enemy->n8[1];
     }
 }
 
