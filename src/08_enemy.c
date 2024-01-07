819917c43648a9b976821590cd426e3b1aca0449
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-08 08:57:43 +0900

    潜水艦のクリッピング

--- src/08_enemy.c
+++ src/08_enemy.c
@@ -32,7 +32,7 @@ static uint8_t* get_init_ptn(uint8_t type)
 
 // 属性定義
 const uint8_t attr_bomb[1] = { 0x85 };
-const uint8_t attr_marineLR[3] = { 0x80, 0x80, 0x80 };
+const uint8_t attr_marineLR[3] = { 0x00, 0x00, 0x00 };
 const uint8_t attr_marineRL[3] = { 0xC0, 0xC0, 0xC0 };
 
 static uint8_t* get_init_attr(uint8_t type)
@@ -155,6 +155,44 @@ void move_marineLR(Enemy* enemy) __z88dk_fastcall
 {
     enemy->vx.value = 0x00C0;
     enemy->n8[0]++;
+
+    // クリッピング
+    switch (enemy->flag) {
+        case 1:
+            if (8 <= enemy->x.raw[1]) {
+                VGS0_ADDR_OAM[SP_ENEMY + enemy->si + 2].attr = 0x80;
+                enemy->flag++;
+            }
+            break;
+        case 2:
+            if (24 <= enemy->x.raw[1]) {
+                VGS0_ADDR_OAM[SP_ENEMY + enemy->si + 1].attr = 0x80;
+                enemy->flag++;
+            }
+            break;
+        case 3:
+            if (28 <= enemy->x.raw[1]) {
+                VGS0_ADDR_OAM[SP_ENEMY + enemy->si].attr = 0x80;
+                enemy->flag++;
+            }
+            break;
+        case 4:
+            if (enemy->x.raw[1] < 28) {
+                enemy->flag++;
+            }
+            break;
+        case 5:
+            if (8 <= enemy->x.raw[1]) {
+                VGS0_ADDR_OAM[SP_ENEMY + enemy->si + 2].attr = 0x00;
+                enemy->flag++;
+            }
+            break;
+        case 6:
+            if (16 <= enemy->x.raw[1]) {
+                enemy->flag = 0; // 画面アウト
+            }
+            break;
+    }
 
     // プロペラを回転
     if (0 == (enemy->n8[0] & 3)) {
