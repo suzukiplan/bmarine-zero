e8727947fdf2bbb4fa53e15adc88cdbb3d968de9
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-08 17:28:47 +0900

    効果音追加

--- src/08-00_bomber.c
+++ src/08-00_bomber.c
@@ -6,6 +6,7 @@ static const uint8_t ptn[10] = {0xA0, 0xA3, 0xA6, 0xA9, 0xAC, 0xD0, 0xD3, 0xD6, 
 void move_bomber(Enemy* enemy) __z88dk_fastcall
 {
     if (1 == enemy->flag) {
+        vgs0_se_play(7);
         int8_t vx = (int8_t)get_random(&GV->ridx);
         int8_t vy = (int8_t)get_random(&GV->ridx);
         enemy->vx.value = vx;
