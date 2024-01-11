3f850a11d8aed2e060af48761987bac3475f1693
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-11 20:10:59 +0900

    稀に雷撃を斜め方向に飛ばす

--- src/08-03_thunder.c
+++ src/08-03_thunder.c
@@ -3,17 +3,31 @@
 // 3: 雷撃
 void move_thunder(Enemy* enemy) __z88dk_fastcall
 {
+    if (enemy->flag == 1) {
+        enemy->flag = 2;
+        if (get_random(&GV->ridx) < 0x20) {
+            enemy->vx.value = get_random(&GV->ridx);
+            if (get_random(&GV->ridx) < 0x80) {
+                enemy->vx.value = -enemy->vx.value;
+            } else {
+                VGS0_ADDR_OAM[enemy->si[0]].attr |= 0x40;
+            }
+            enemy->n8[2] = 0x1E;
+        } else {
+            enemy->n8[2] = 0x1C;
+        }
+    }
     enemy->check = 1;
     enemy->n8[0]++;
     enemy->n8[0] &= 3;
     if (0 == enemy->n8[0]) {
         enemy->n8[1] = 1 - enemy->n8[1];
-        VGS0_ADDR_OAM[enemy->si[0]].ptn = 0x1C + enemy->n8[1];
+        VGS0_ADDR_OAM[enemy->si[0]].ptn = enemy->n8[2] + enemy->n8[1];
     }
     if (-900 < (int16_t)enemy->vy.value) {
         enemy->vy.value -= 7;
     }
-    if (192 < enemy->y.raw[1]) {
+    if (192 < enemy->y.raw[1] || 248 <= enemy->x.raw[1]) {
         enemy->flag = 0;
     }
 }
