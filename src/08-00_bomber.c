75ad4cd82b0c150c82bbae2eceea8887f8c5b4af
suzukiplan <suzukiplan.ys@gmail.com>
2024-01-17 10:24:06 +0900

    画面上部の爆発の当たり判定は小さくする

--- src/08-00_bomber.c
+++ src/08-00_bomber.c
@@ -17,7 +17,16 @@ void move_bomber(Enemy* enemy) __z88dk_fastcall
     }
     enemy->flag++;
     if (0 == (enemy->flag & 3)) {
-        enemy->check = 1;
+        if (0 == enemy->check) {
+            enemy->check = 1;
+            // 画面上部の爆発の当たり判定は小さくする
+            if (enemy->y.raw[1] < 100) {
+                enemy->hit.x = 10;
+                enemy->hit.y = 10;
+                enemy->hit.width = 4;
+                enemy->hit.height = 4;
+            }
+        }
         enemy->n8[0]++;
         if (enemy->n8[0] < 10) {
             VGS0_ADDR_OAM[enemy->si[0]].ptn = ptn[enemy->n8[0]];
